ddeeb0ff30e231766e75a96665e1eae8e1b730de
Mateusz <michalikkontakt@gmail.com>
2022-05-05 00:41:42 +0200

    added totaly fresh logic with product and subpages

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,16 +4,21 @@ import { newsletterLogic } from "./modules/newsletter.js";
 import { contactLogic } from "./modules/contact.js";
 import { scrollButtonFunctions } from "./modules/scroll.js";
 import { type } from "./modules/typewriter.js";
-import { rendererLogic } from "./modules/products/render-products.js";
+import { rendererLogic, rendererAll } from "./modules/products/render-products.js";
 const mainFunctions = () => {
     navLogic();
-    rendererLogic('shoes');
-    rendererLogic('clothes');
-    rendererLogic('accesories');
-    recommendedLogic();
-    newsletterLogic();
-    contactLogic();
     scrollButtonFunctions();
-    type();
+    if (document.body.getAttribute('data-page') === 'main') {
+        rendererLogic('shoes');
+        rendererLogic('clothes');
+        rendererLogic('accesories');
+        recommendedLogic();
+        newsletterLogic();
+        type();
+        contactLogic();
+    }
+    else if (document.body.getAttribute('data-page') === 'products') {
+        rendererAll();
+    }
 };
 document.addEventListener('DOMContentLoaded', mainFunctions);
