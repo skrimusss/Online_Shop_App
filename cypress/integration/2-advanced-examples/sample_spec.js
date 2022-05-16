b834539305e40601ce9e2571ba08dacbf32e98e5
Mateusz <michalikkontakt@gmail.com>
2022-05-16 19:14:29 +0200

    removed cypress from github

--- cypress/integration/2-advanced-examples/sample_spec.js
+++
@@ -1,6 +0,0 @@
-describe('My First Test', () => {
-    it('Checking shop', () => {
-        cy.visit('http://127.0.0.1:5500/sneakers.html?')
-        cy.contains('image-box__button').click()
-    })
-})
